5e0685fd76e169ec5bc296bb383cc89c6e51f780
Jad Saade <sjad@MacBook-Air-de-Jad.local>
2023-09-26 14:51:32 +0200

    database

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  'cloud_14ki', // Database name
+  'clouduser', // User
+  'id5J4bnvJov08uxfnQc9tbOTozauOr9u', // Password
   {
-    host: '', // Host
+    host: 'pg-ck9d0f6gtj9c73c4g4k0-a', // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
